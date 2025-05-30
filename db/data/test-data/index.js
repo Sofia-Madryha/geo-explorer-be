0202cfdfcabc3efd9db0ea8aecdfe484ec45a90e
naydenhristov <nhristov21@gmail.com>
2025-05-30 16:51:35 +0100

    questions and answers tables created and data inserted

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -1,3 +1,5 @@
 exports.categoriesData = require("./categories.js");
 exports.subCategoriesData = require("./sub-categories.js");
-exports.learningCardsData = require("./learning-card.js")
+exports.learningCardsData = require("./learning-card.js");
+exports.answersMultipleChoicesData = require("./answers-multiple-choices.js");
+exports.questionsMultipleChoicesData = require("./questions-multiple-answers.js");
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,8 +1,21 @@
+const format = require("pg-format");
 const db = require("../connection");
 
-const seed = ({ categoriesData, subCategoriesData, learningCardsData }) => {
+const seed = ({
+  categoriesData,
+  subCategoriesData,
+  learningCardsData,
+  answersMultipleChoicesData,
+  questionsMultipleChoicesData,
+}) => {
   return db
     .query(`DROP TABLE IF EXISTS learning_cards;`)
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS answers_multiple_choices;`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS questions_multiple_answers;`);
+    })
     .then(() => {
       return db.query(`DROP TABLE IF EXISTS sub_categories;`);
     })
@@ -12,7 +25,7 @@ const seed = ({ categoriesData, subCategoriesData, learningCardsData }) => {
     .then(() => {
       return db.query(`CREATE TABLE categories (
             category_id SERIAL PRIMARY KEY,
-            category_name VARCHAR(50) NOT NULL,
+            category_name VARCHAR(100) NOT NULL,
             img_url VARCHAR(1000)
             );`);
     })
@@ -25,6 +38,23 @@ const seed = ({ categoriesData, subCategoriesData, learningCardsData }) => {
               );`);
     })
     .then(() => {
+      return db.query(`CREATE TABLE questions_multiple_answers (
+            question_mc_id SERIAL PRIMARY KEY,
+            continent VARCHAR(50),
+            sub_category_id INT REFERENCES sub_categories(sub_category_id),
+            level VARCHAR(50),
+            question_text TEXT
+        );`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE answers_multiple_choices (
+            answer_mc_id SERIAL PRIMARY KEY,
+            question_mc_id INT REFERENCES questions_multiple_answers(question_mc_id),
+            multiple_choice_text VARCHAR(500),
+            correct_answer VARCHAR(100)
+        );`);
+    })
+    .then(() => {
       return db.query(`CREATE TABLE learning_cards (
             card_id SERIAL PRIMARY KEY,
             continent VARCHAR(50),
@@ -35,38 +65,85 @@ const seed = ({ categoriesData, subCategoriesData, learningCardsData }) => {
         );`);
     })
     .then(() => {
-      return categoriesData.map((category) => {
-        return db.query(
-          `INSERT INTO categories(category_name, img_url) VALUES ($1, $2) RETURNING *;`,
-          [category.category_name, category.img_url]
-        );
+      const formattedCategory = categoriesData.map((category) => {
+        return [category.category_name, category.img_url];
       });
+      const insertCategoriesQuery = format(
+        `INSERT INTO categories(category_name, img_url) VALUES %L`,
+        formattedCategory
+      );
+      return db.query(insertCategoriesQuery);
     })
     .then(() => {
-      return subCategoriesData.map((sub_category) => {
-        return db.query(
-          `INSERT INTO sub_categories(sub_category_name, category_id, img_url) VALUES ($1, $2, $3) RETURNING *;`,
-          [
-            sub_category.sub_category_name,
-            sub_category.category_id,
-            sub_category.img_url,
-          ]
-        );
+      const formattedSubCategory = subCategoriesData.map((sub_category) => {
+        return [
+          sub_category.sub_category_name,
+          sub_category.category_id,
+          sub_category.img_url,
+        ];
       });
+      const insertSubCategoriesQuery = format(
+        `INSERT INTO sub_categories(sub_category_name, category_id, img_url) VALUES %L`,
+        formattedSubCategory
+      );
+      return db.query(insertSubCategoriesQuery);
     })
     .then(() => {
-      learningCardsData.map((learningCard) => {
-        return db.query(
-          `INSERT INTO learning_cards(continent, sub_category_id, title, description, img_url) VALUES($1, $2, $3, $4, $5)`,
-          [
-            learningCard.continent,
-            learningCard.sub_category_id,
-            learningCard.title,
-            learningCard.description,
-            learningCard.img_url,
-          ]
-        );
+      const formattedQuestionsMC = questionsMultipleChoicesData.map((question) => {
+        return [
+          question.continent,
+          question.sub_category_id,
+          question.level,
+          question.question_text
+        ];
       });
+      const insertQuestionsMCQuery = format(
+        `INSERT INTO questions_multiple_answers (
+            continent,
+            sub_category_id,
+            level,
+            question_text
+        ) VALUES %L`,
+        formattedQuestionsMC
+      );
+      return db.query(insertQuestionsMCQuery);
+    })
+    .then(() => {
+      const formattedAnswersMC = answersMultipleChoicesData.map((answer) => {
+        return [
+          answer.question_mc_id,
+          answer.multiple_choice_text,
+          answer.correct_answer
+        ];
+      });
+      const insertAnswersMCQuery = format(
+        `INSERT INTO answers_multiple_choices (
+            question_mc_id,
+            multiple_choice_text,
+            correct_answer
+        ) VALUES %L`,
+        formattedAnswersMC
+      );
+      return db.query(insertAnswersMCQuery);
+    })
+    .then(() => {
+      const formattedLearningCards = learningCardsData.map((learningCard) => {
+        return [
+          learningCard.continent,
+          learningCard.sub_category_id,
+          learningCard.title,
+          learningCard.description,
+          learningCard.img_url,
+        ];
+      });
+      const insertLearningCardsQuery = format(
+        `INSERT INTO learning_cards(continent, sub_category_id, title, description, img_url) VALUES %L`,
+        formattedLearningCards
+      );
+      return db.query(insertLearningCardsQuery);
+    })
+    .then(() => {
+      console.log("Seed completed!");
     })
     .then(() => {
       console.log("Seed completed!");
