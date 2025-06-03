8a853bccee4a8bddbe78006f0a0ce404eb4d39d7
catarinasapateiro <catarinasapateiro@hotmail.com>
2025-06-03 12:19:11 +0100

    get sub-categories and get multichoice-qa

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,6 +31,70 @@ describe("1. GET/ Categories", () => {
   });
 });
 
+describe(" GET/ Sub-Categories", () => {
+  test("1 200-responds with an array of sub-categories", () => {
+    return request(app)
+      .get("/api/subcategories")
+      .expect(200)
+      .then(({ body: { subcategories } }) => {
+        expect(subcategories.length).toBe(4);
+        subcategories.forEach((subcategory) => {
+          expect(subcategory).toMatchObject({
+            sub_category_id: expect.any(Number),
+            category_id: expect.any(Number),
+            sub_category_name: expect.any(String),
+            img_url: expect.any(String),
+          });
+        });
+      });
+  });
+});
+
+describe(" GET/ Multi-choice questions and answers", () => {
+  test.only("200-responds with an array of multi-choice questions and answers", () => {
+    return request(app)
+      .get("/api/multichoice-qa")
+      .expect(200)
+      .then(({ body: { multichoice_qa } }) => {
+        expect(multichoice_qa.length).toBe(23);
+        multichoice_qa.forEach((item) => {
+          expect(item).toMatchObject({
+            question_mc_id: expect.any(Number),
+            continent: expect.any(String),
+            sub_category_id: expect.any(Number),
+            level: expect.any(String),
+            question_text: expect.any(String),
+            answer_mc_id: expect.any(Number),
+            multiple_choice_text: expect.any(String),
+            correct_answer: expect.any(String),
+          });
+        });
+      });
+  });
+  test.skip("200-responds with an array of multi-choice questions and answers", () => {
+    return request(app)
+      .get(
+        "/api/multichoice-qa?level=beginner&&continent=asia&&sub_category_id=4"
+      )
+      .expect(200)
+      .then(({ body: { multichoice_qa } }) => {
+        expect(multichoice_qa.length).toBe(23);
+        multichoice_qa.forEach((item) => {
+          expect(item).toMatchObject({
+            question_mc_id: expect.any(Number),
+            continent: "asia",
+            sub_category_id: 4,
+            level: "Beginner",
+            question_text: expect.any(String),
+            answer_mc_id: expect.any(Number),
+            multiple_choice_text: expect.any(String),
+            correct_answer: expect.any(String),
+          });
+        });
+      });
+  });
+});
+
 describe("2. 404: Not Found - BAD url-error", () => {
   test("2. 404-attemping to access a non-existing endpoint ", () => {
     return request(app)
--- routes/api-router.js
+++ routes/api-router.js
@@ -1,10 +1,14 @@
 const { getApi } = require("../controllers/api-controller");
 const categoriesRouter = require("./categories-router");
+const multiChoiceQARouter = require("./multichoice-qa-router");
+const subCategoriesRouter = require("./subcategories-router");
 
 const apiRouter = require("express").Router();
 
 //Routes
 apiRouter.get("/", getApi);
 apiRouter.use("/categories", categoriesRouter);
+apiRouter.use("/subcategories", subCategoriesRouter);
+apiRouter.use("/multichoice-qa", multiChoiceQARouter);
 
 module.exports = apiRouter;
