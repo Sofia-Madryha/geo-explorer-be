26a56eb8cb0579b8b8aab7a8d40edffb1d057919
catarinasapateiro <catarinasapateiro@hotmail.com>
2025-06-03 14:26:41 +0100

    fix import

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -1,7 +1,7 @@
 exports.categoriesData = require("./categories.js");
 
 exports.subCategoriesData = require("./sub_categories.js");
-exports.learningCardsData = require("./learning-card.js");
+exports.learningCardsData = require("./learning_cards.js");
 exports.answersMultipleChoicesData = require("./answers-multiple-choices.js");
 exports.questionsMultipleChoicesData = require("./questions-multiple-choices.js");
 
