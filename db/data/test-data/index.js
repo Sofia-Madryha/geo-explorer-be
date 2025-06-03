457461726e3755a92890710c74e6146ec46cbbb9
sofimadryha <75246511+Sofia-Madryha@users.noreply.github.com>
2025-06-03 17:02:06 +0300

    Update index.js

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -4,6 +4,6 @@ exports.subCategoriesData = require("./sub_categories.js");
 exports.learningCardsData = require("./learning_cards.js");
 exports.answersMultipleChoicesData = require("./answers-multiple-choices.js");
 exports.questionsMultipleChoicesData = require("./questions-multiple-choices.js");
-
+exports.questionsMatchingPairsData = require("./questions-matching-pairs.js")
 exports.answersMatchingPairsData = require("./answers-matching-pairs.js");
 exports.usersData = require("./users.js");
