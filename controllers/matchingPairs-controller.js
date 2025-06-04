04957a5924d02623e5dfb90243c607d255a2c9cb
Wai Yan Sze <szewaiyan@Wais-MacBook-Air.local>
2025-06-04 09:31:42 +0100

    revised database for question

---
+++ controllers/matchingPairs-controller.js
@@ -0,0 +1,22 @@
+const {
+  selectMatchingPairsByFilters,
+} = require("../models/matchingPairs-model");
+
+exports.getMatchingPairsByFilters = (req, res, next) => {
+  const { category_id, continent, level } = req.query;
+
+  selectMatchingPairsByFilters(category_id, continent, level)
+    .then((matchingPairs) => {
+      if (matchingPairs.length === 0) {
+        return res.status(404).json({ msg: "404 Not Found" });
+      }
+      res.status(200).json({ matchingPairs });
+    })
+    .catch((err) => {
+      if (err.status && err.msg) {
+        res.status(err.status).json({ msg: err.msg });
+      } else {
+        next(err);
+      }
+    });
+};
