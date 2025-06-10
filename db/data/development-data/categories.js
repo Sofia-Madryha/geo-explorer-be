303a25839bc4090f1dcf387a06aabbe28d1c2e70
Wai Yan Sze <szewaiyan@Wais-MacBook-Air.local>
2025-06-10 13:46:10 +0100

    Finished the endpoint of map question

--- db/data/development-data/categories.js
+++ db/data/development-data/categories.js
@@ -1,12 +1,12 @@
 module.exports = [
   {
     category_id: 1,
-    category_name: "nature",
+    category_name: "Nature",
     img_url: "https://sb.ecobnb.net/app/uploads/sites/3/2020/01/nature.jpg",
   },
   {
     category_id: 2,
-    category_name: "territories",
+    category_name: "Territories",
     img_url:
       "https://www.worldatlas.com/upload/f4/e0/49/shutterstock-1665254809.png",
   },
